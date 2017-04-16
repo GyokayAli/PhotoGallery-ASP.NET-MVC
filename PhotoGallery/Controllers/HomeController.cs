099d265a1cac846f9447c60888c3f9f3f0d536fd
GyokayAli <gyokay.metin.ali@gmail.com>
2017-04-16 22:46:18 +0300

    Added views (Register & Login)

--- PhotoGallery/Controllers/HomeController.cs
+++ PhotoGallery/Controllers/HomeController.cs
@@ -13,23 +13,24 @@ namespace PhotoGallery.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
 
         public ActionResult Gallery()
         {
             ViewBag.Message = "Your gallery page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Login";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Register";
 
             return View();
         }
