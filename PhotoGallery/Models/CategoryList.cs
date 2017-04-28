bb1ced2207e8c7d766dd4e8f1d9084a7cb09dec6
GyokayAli <gyokay.metin.ali@gmail.com>
2017-04-28 20:27:21 +0300

    Edited CategoryList

--- PhotoGallery/Models/CategoryList.cs
+++ PhotoGallery/Models/CategoryList.cs
@@ -10,21 +10,21 @@ namespace PhotoGallery.Models
 {
     public class CategoryList
     {
-        PhotoGalleryEntities db = new PhotoGalleryEntities();
-
         public int CategoryItemId { get; set; }
         public IEnumerable<SelectListItem> Categories
         {
             get
             {
-                return db.Categories.Select(x =>
+                return new[] { new SelectListItem { Value = "0", Text = "Select Category", Selected=true }}.Concat(
+                    new PhotoGalleryEntities().Categories.Select(x =>
                       new SelectListItem
                       {
                           Value = x.ID.ToString(),
                           Text = x.CATEGORY_NAME
-                      }).ToList();
+                      }).ToList()
+                );
             }
-            set { }
+            set {}
         }
     }
 }
